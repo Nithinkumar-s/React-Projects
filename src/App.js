8005f7ad7e2a77699ada9a07bb6b93d14a36c9a0
Nithinkumar <nithinplavila@gmail.com>
2023-01-14 14:51:38 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+import {useState } from 'react';
+
 
 function App() {
+
+  const [calc,setCalc] =  useState('');
+  const [result,setResult] =  useState('');
+  const op = ['+','-','*','/','.']
+
+  function createDigit(){
+    const digits=[];  
+    for (let i = 1; i < 10; i++) {
+      digits.push(<button key={i} onClick={() => buttonPressed(i.toString())} >{i}</button>);
+    }
+    return digits;
+  };
+   
+  const buttonPressed = value =>{
+    console.log('value');
+    if(op.includes(value) && calc === '' || op.includes(value)  && op.includes(calc.slice(-1)) ){
+      return;
+    } 
+      setCalc(calc + value)
+  }
+  const clearPressed  = value => {
+    if(value === 0 && calc != ''){
+    setCalc('')
+    setResult('')
+    }
+   else if(value === 1 && calc != ''){
+    const newvalue = calc.slice(0,-1)
+    setCalc(newvalue)
+    }
+
+  }
+  const resultValue = ()=>{
+    let result = parseFloat(eval(calc))
+    setResult(Number(result));
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      
+            <div className="data"> 
+            <div>{calc || 0}</div>
+            <div className='resultData'>{result|| ''}</div>
+            </div>
+            <div className="display">
+                  <div className="numbers">
+                    
+                  <button >❤</button>
+                  <button onClick={() => clearPressed(0)}>CLEAR</button>
+                  
+                  <button onClick={() => clearPressed(1)}>DEL</button>
+                    {createDigit()}
+                    <button onClick={() => buttonPressed('0')} >0</button>
+                    <button onClick={() => buttonPressed('.')} >.</button>
+                    <button className='sum' onClick={() => resultValue()}>=</button>
+                  </div>
+                  <div className="operators">
+                    <button onClick={() => buttonPressed('+')} >+</button>
+                    <button onClick={() => buttonPressed('-')} >-</button>
+                    <button onClick={() => buttonPressed('*')} >*</button>
+                    <button onClick={() => buttonPressed('/')} >/</button>
+                  </div>
+
+            </div>
+      
+      
     </div>
   );
 }
